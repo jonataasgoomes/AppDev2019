9e6ad6685e233ae6edc65d43fcf5c3e7b72b97d0
Jônatas <jonataasgoomes@gmail.com>
2019-04-04 19:07:17 -0300

    Implementado a funcionalidade dos botões
    Adotar , Ajudar , Cadastrar Animal e Login - Agora todos os botões abrem uma nova Activity para as próximas funcionalidades

--- app/src/main/java/br/unb/meau/SplashActivity.java
+++ app/src/main/java/br/unb/meau/SplashActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import java.util.Objects;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -13,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         new Handler().postDelayed(new Runnable() {
