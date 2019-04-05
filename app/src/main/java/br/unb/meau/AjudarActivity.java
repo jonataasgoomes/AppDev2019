0e37d6541fbdd4901520629c7b4230f8660fd398
Jônatas <jonataasgoomes@gmail.com>
2019-04-05 20:56:49 -0300

    Salvando modificações básicas
    Fim tela introdução.

--- app/src/main/java/br/unb/meau/AjudarActivity.java
+++ app/src/main/java/br/unb/meau/AjudarActivity.java
@@ -9,5 +9,6 @@ public class AjudarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajudar);
+        getSupportActionBar().setTitle("Ajudar");
     }
 }
