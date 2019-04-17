e441de510862736334eaa879789f5dcc1dfc63d4
Jônatas <jonataasgoomes@gmail.com>
2019-04-16 21:19:01 -0300

    main toolbar

--- app/src/main/java/br/unb/meau/activity/MainActivity.java
+++ app/src/main/java/br/unb/meau/activity/MainActivity.java
@@ -1,16 +1,23 @@
 package br.unb.meau.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import br.unb.meau.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-
+    DrawerLayout drawerLayout;
     private Button btnLogin;
     private Button btnAdotar;
     private Button btnAjudar;
@@ -21,8 +28,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Remove a action bar
-        //Objects.requireNonNull(getSupportActionBar()).hide();
+        //configura toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.main_actity);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.drawer);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar ,
+                R.string.drawer_open
+                ,R.string.drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
 
         //btnLogin = findViewById(R.id.buttonLogin);
         btnAdotar = findViewById(R.id.buttonPets);
@@ -59,5 +77,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        closeDrawer();
+        return true;
+    }
+
+    private void closeDrawer() {
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+    private void openDrawer(){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            closeDrawer();
+        }
+        super.onBackPressed();
     }
 }
